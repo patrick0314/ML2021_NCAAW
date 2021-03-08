6cff887a86194b3bceebba69ee32e29862e79494
patrick0314 <b05901174@ntu.edu.tw>
2021-03-08 14:12:05 +0800

    .

---
+++ regression.py
@@ -0,0 +1,75 @@
+import os
+import numpy as np
+import pandas as pd
+import torch
+import torch.utils.data as Data
+import torch.nn as nn
+
+from ReadData import *
+
+# Data Preprocess
+print('=== data process ===')
+compactresult = CompactResult() # [1386, 8]
+detailedresult = DetailedResult() # [630, 34] = [Season, DayNum, WTeamID, WScore, LTeamID, LScore, WLoc ... LOR, LDR, LAst, LTO, LStl, LBlk, LPF]
+regularcompactresult = RegularCompactResult() # [112183, 8]
+regulardetailedresult = RegularDetailedResult() # [56793, 34]] = [Season, DayNum, WTeamID, WScore, LTeamID, LScore, WLoc ... LOR, LDR, LAst, LTO, LStl, LBlk, LPF]
+tmp = pd.concat([compactresult, regularcompactresult], axis=0, ignore_index=True)
+tmp = tmp.drop(['DayNum', 'WScore', 'LScore', 'NumOT', 'WLoc'], axis=1)
+tmp1 = pd.concat([detailedresult, regulardetailedresult], axis=0, ignore_index=True) # [57423, 34]
+tmp1 = tmp1.drop(['DayNum', 'WScore', 'LScore', 'NumOT', 'WLoc', 'WFGM', 'WFGA', 'WFGM3', 'WFGA3', 'WFTM', 'WFTA', 'WOR', 'WDR', 'WAst', 'WTO', 'WStl', 'WBlk', 'WPF'], axis=1)
+tmp1 = tmp1.drop(['LFGM', 'LFGA',  'LFGA', 'LFGM3', 'LFGA3', 'LFTM', 'LFTA', 'LOR', 'LDR', 'LAst', 'LTO', 'LStl', 'LBlk', 'LPF'], axis=1)
+detail = pd.concat([tmp, tmp1], axis=0, ignore_index=True)
+detail = detail.to_numpy()
+detail = detail.astype(float) # [170992, 3]
+detail = np.concatenate((detail, np.ones((detail.shape[0], 1))), axis=1) # add bias [170992, 4]
+
+lable = np.zeros(len(detail)) # [170992, 1]
+for i in range(len(detail)):
+    if int(detail[i, 1] ) < int(detail[i, 2]):
+        lable[i] = 1
+    else:
+        lable[i] = 0
+
+# 
+x = detail
+y = lable
+
+# parameter
+print('=== parameter setting ===')
+w = np.zeros(len(x[0]))
+lr = 1
+iteration = 10000
+s_grad = np.zeros(len(x[0]))
+x_t = x.transpose()
+
+# linear regression training
+print('=== start regression ===')
+for i in range(iteration):
+    if i % 100 == 0:
+        print('iteration: ', i, '  ', w)
+    pred = np.dot(x, w)
+    loss = pred - y
+    grad = 2 * np.dot(x_t, loss)
+    s_grad += grad**2
+    ada = np.sqrt(s_grad)
+    w = w - lr * grad / ada
+
+print('=== regression complete ===')
+
+# test
+print('=== start prediction ===')
+output = Output()
+output = output
+output = output.to_numpy()
+
+for i in range(len(output)):
+    S = int(output[i][0][0:4])
+    W = int(output[i][0][5:9])
+    L = int(output[i][0][10:])
+    x = np.array([S, W, L, 1.0])
+    pred = np.dot(x, w)
+    output[i][1] = pred
+
+df = pd.DataFrame(output, columns=['ID', 'Pred'])
+df.to_csv('regression.csv', index=0)
+
